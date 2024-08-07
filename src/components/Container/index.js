9d9a530be413c922b24113c848fc62b7be3a71f1
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-06 22:53:42 -0300

    Componente Container criado

---
+++ src/components/Container/index.js
@@ -0,0 +1,13 @@
+import styles from "./Container.module.css"
+
+function Container(){
+    return (
+        <section className={styles.container}>
+            Conteudo
+        </section>
+    )
+
+
+}
+
+export default Container;

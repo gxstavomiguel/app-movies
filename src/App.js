c2ba5087bb9e15ec53c8eb6e1af966b81f7198cb
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-07 14:27:20 -0300

    Lógica de gerar cards de vídeos refeita

--- src/App.js
+++ src/App.js
@@ -6,15 +6,59 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import videos from "./json/videos.json";
 
+const categories = [
+  "Geografia",
+  "Como fazer e usar",
+  "Astronomia e Geografia",
+  "Climatologia, Meteorologia, Vegetação",
+  "Geologia e Hidrografia",
+];
+
+function filterCategory(id) {
+  return videos.filter((video) => video.category === categories[id]);
+}
+
 function App() {
   return (
     <>
       <Header />
       <Banner image="favoritos" />
       <Container>
-      <Category category="Geografia">
-        { videos.map((video) => <Card id={video.id} key={video.id} />)}
-      </Category>
+
+
+        <Category category="Geografia">
+          {filterCategory(0).map((video) => (
+            <Card id={video.id} key={video.id} />
+          ))}
+        </Category>
+
+        <Category category="Como fazer e usar">
+          {filterCategory(1).map((video) => (
+            <Card id={video.id} key={video.id} />
+          ))}
+        </Category>
+
+        <Category category="Astronomia e Geografia">
+          {filterCategory(0).map((video) => (
+            <Card id={video.id} key={video.id} />
+          ))}
+        </Category>
+
+        <Category category="Climatologia, Meteorologia, Vegetação">
+          {filterCategory(0).map((video) => (
+            <Card id={video.id} key={video.id} />
+          ))}
+        </Category>
+
+        <Category category="Geologia e Hidrografia">
+          {filterCategory(0).map((video) => (
+            <Card id={video.id} key={video.id} />
+          ))}
+        </Category>
+
+
+
+
       </Container>
       <Footer />
     </>

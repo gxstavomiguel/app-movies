a53255cf1c674410cfd952b8e27dfe7f0c51f94f
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-07 00:15:04 -0300

    Melhorias

--- src/App.js
+++ src/App.js
@@ -3,29 +3,19 @@ import Card from "./components/Card";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/db.json"
-
+import videos from "./json/db.json";
 
 function App() {
   return (
     <>
       <Header />
-      <Banner image="favoritos"/>
+      <Banner image="favoritos" />
       <Container>
-
-
-       <section className="cards">
-
-        {
-          videos.map(video => {
-            return <Card id={video.id} key={video.id}/>
-          })
-        }
-
-       </section>
-
-
-
+        <section className="cards">
+          {videos.map((video) => {
+            return <Card id={video.id} key={video.id} />;
+          })}
+        </section>
       </Container>
       <Footer />
     </>

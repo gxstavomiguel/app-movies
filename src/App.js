bf89762ceded78e0a780f16622b69006171c003b
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-07 14:09:25 -0300

    json atualizado com mais conteúdo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from "./components/Card";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/db.json";
+import videos from "./json/videos.json";
 
 function App() {
   return (

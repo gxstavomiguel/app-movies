c136858063be2706040bcf22bd03b95b8ed29e12
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-06 23:59:18 -0300

    Deixando as capas e links dinámicos

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,15 @@
 import styles from "./Card.module.css"
 
-function Card(){
+function Card({ id }){
     return (
         <section className={ styles.card }>
             <a  
-            href="https://www.youtube.com/watch?v=B_nq7VTJZds"
+            href={`https://www.youtube.com/watch?v=${id}`}
+            // B_nq7VTJZds
             rel="noreferrer noopener"
             target="_blank">
-                <img src="https://img.youtube.com/vi/B_nq7VTJZds/mqdefault.jpg"    
+                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} 
+                // B_nq7VTJZds   
                 alt="Capa"/>
             </a>
 

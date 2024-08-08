f336b87495730816bb988b71adc7d632ea33c4dd
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-08 01:29:54 -0300

    Tornando dinamico o acesso aos videos na página watch

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,16 @@
-import styles from "./Card.module.css"
+import { Link } from "react-router-dom";
+import styles from "./Card.module.css";
 
-function Card({ id }){
-    return (
-        <section className={ styles.card }>
-            <a  
-            href={`https://www.youtube.com/watch?v=${id}`}
-           
-            rel="noreferrer noopener"
-            target="_blank">
-                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} 
-                  
-                alt="Capa"/>
-            </a>
-        </section>
-    );
+function Card({ id }) {
+  return (
+    <section className={styles.card}>
+      <Link to={`/watch/${id}`}>
+        <img
+          src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
+          alt="Capa"/>
+      </Link>
+    </section>
+  );
 }
 
 export default Card;
--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -1,15 +1,27 @@
+import { useParams } from "react-router-dom";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import styles from "./Watch.module.css";
+import videos from "../../json/videos.json"
 
 function Watch() {
+  const params = useParams()
+  const video = videos.fund((video) => {return video.id === params.id });
+  console.log(video)
+
   return (
     <>
       <Header />
       <Container>
         <section className={styles.watch}>
-          <iframe width="854" height="480" src="https://www.youtube.com/embed/2y1qW_aH0gQ?si=w6fGExkJNQmI30m4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <iframe 
+            width="854" height="480" 
+            src={`https://www.youtube.com/embed/${video.id}`}
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" 
+            allowfullscreen></iframe>
         </section>
       </Container>
       <Footer />

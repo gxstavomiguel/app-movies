f22dde9a024df762b026b0ada19e2ff5b8d4d181
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-08 01:10:23 -0300

    Página criada para rotas não existentes e melhorias no css

--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -9,7 +9,7 @@ function Watch() {
       <Header />
       <Container>
         <section className={styles.watch}>
-          <h1>Assistir</h1>
+          <iframe width="854" height="480" src="https://www.youtube.com/embed/2y1qW_aH0gQ?si=w6fGExkJNQmI30m4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </section>
       </Container>
       <Footer />

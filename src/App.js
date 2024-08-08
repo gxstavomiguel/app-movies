ec8b946733ac5b242d8b3e52fb15d815ee6d3903
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-08 00:36:26 -0300

    Organizando pages e iniciando as rotas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import Banner from "./components/Banner";
-import Card from "./components/Card";
-import Category, { categories, filterCategory } from "./components/Category";
-import Container from "./components/Container";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Banner image="favoritos" />
-      <Container>
-        {categories.map((category, index) => (
-          <Category category={category}>
-            {filterCategory(index).map((video) => (
-              <Card id={video.id} key={video.id} />
-            ))}
-          </Category>
-        ))}
-      </Container>
-      <Footer />
-    </>
+    <Home />
   );
 }
 
---
+++ src/pages/Watch/index.js
@@ -0,0 +1,11 @@
+import styles from "./Watch.module.css";
+
+function Watch() {
+  return (
+    <section className={styles.watch}>
+      <h1>Assistir</h1>
+    </section>
+  );
+}
+
+export default Watch;

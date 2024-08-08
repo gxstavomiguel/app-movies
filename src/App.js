78edce6d832e0c2ce167f975b37d784ca80064fb
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-08 00:25:59 -0300

    Escopo de categoria reorganizado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
-import Category from "./components/Category";
+import Category, { categories, filterCategory } from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/videos.json";
-
-const categories = [
-  "Geografia",
-  "Como fazer e usar",
-  "Astronomia e Geografia",
-  "Climatologia, Meteorologia, Vegetação",
-  "Geologia e Hidrografia",
-];
-
-function filterCategory(id) {
-  return videos.filter((video) => video.category === categories[id]);
-}
 
 function App() {
   return (

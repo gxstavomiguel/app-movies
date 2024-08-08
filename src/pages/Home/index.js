4a5c9834256a237f2c900f333c527e02e6fc4b40
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-08 17:02:11 -0300

    Começando a usar useState

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,40 @@
+import { useState } from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
-import Category, { categories, filterCategory } from "../../components/Category";
+import Category, {
+  categories,
+  filterCategory,
+} from "../../components/Category";
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 function Home() {
+  //let nomeDigitado = '';
+  function pegarNome(event) {
+    console.log(event.target.value);
+    //nomeDigitado = event.target.value;
+    setNome(event.target.value);
+  }
+
+  // Hook = gancho -> useState
+
+  const [nome, setNome] = useState();
+
   return (
     <>
       <Header />
       <Banner image="favoritos" />
       <Container>
-        {categories.map((category, index) => (
+        <input type="text" placeholder="Nome" value="" onChange={pegarNome} />
+        <h2>{nome}</h2>
+        {/* {categories.map((category, index) => (
           <Category category={category}>
             {filterCategory(index).map((video) => (
               <Card id={video.id} key={video.id} />
             ))}
           </Category>
-        ))}
+        ))} */}
       </Container>
       <Footer />
     </>

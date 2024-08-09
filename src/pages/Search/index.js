7a8f2b0e8c1bcfad60a8829a58ff265437c2be39
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-08 23:48:26 -0300

    Implementando metodo de pesquisa

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,11 +1,22 @@
-import styles from "./Search.module.css"
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import styles from "./Search.module.css";
+import Container from "../../components/Container"
 
-function Search(){
-    return (
+function Search() {
+  return (
+    <>
+      <Header />
+        <Container>
         <section className={styles.search}>
-            <h2>Pesquisar</h2>
-        </section>
-    );
+        <h2>Pesquisar</h2>
+        <input type="search">
+        </input>
+      </section>
+        </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default Search;

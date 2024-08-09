4e65f4c7c518e3d6cb57f057b61548d66ad1dce7
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-09 02:10:37 -0300

    Propriedades do carrossel definidas

---
+++ src/components/Carousel/index.js
@@ -0,0 +1,26 @@
+import Slider from "react-slick";
+import "./carousel.css";
+
+function Carousel({ children }){
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 300,
+        centerMode: false,
+        variableWidth: true,
+        adaptiveHeight: true,
+        //slidesToShow: 3,
+        slidesToScroll: 1,
+      };
+
+
+    return (
+        <div>
+            <Slider {...settings}>
+                { children }
+            </Slider>
+        </div>
+    );
+}
+
+export default Carousel;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,10 +8,10 @@ import Category, {
 import Container from "../../components/Container";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import Carousel from "../../components/Carousel";
 
 function Home() {
   function pegarNome(event) {
-    console.log(event.target.value);
     setNome(event.target.value);
   }
 
@@ -26,9 +26,11 @@ function Home() {
       <Container>
         {categories.map((category, index) => (
           <Category category={category}>
-            {filterCategory(index).map((video) => (
-              <Card id={video.id} key={video.id} />
-            ))}
+            <Carousel>
+              {filterCategory(index).map((video) => (
+                <Card id={video.id} key={video.id} />
+              ))}
+            </Carousel>
           </Category>
         ))}
       </Container>

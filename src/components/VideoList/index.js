101357107c44a174ceb8bc88cda29f9f017666d0
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-09 00:04:27 -0300

    Agora exibe a quantidade de vídeos corretamente

--- src/components/VideoList/index.js
+++ src/components/VideoList/index.js
@@ -1,24 +1,26 @@
-import styles from "./VideoList.module.css"
-import videos from "../../json/videos.json"
+import styles from "./VideoList.module.css";
+import videos from "../../json/videos.json";
+import Card from "../Card";
 
-function VideoList({ videos, emptyHeading}){
+function VideoList({ videos, emptyHeading }) {
+  const count = videos.length;
+  let heading = emptyHeading;
 
-    const count = videos.length
-    let heading = emptyHeading
+  if (count > 0) {
+    const noun = count > 1 ? 'vídeos' : 'vídeo'
+    heading = `${count} ${noun}` 
+  }
 
-    if(count > 0){
-        const noun = count > ? 'vídeos' : 'vídeo';
-        heading = `${count} ${noun}`
-    }
-
-    return(
-        <>
-            <h2>{emptyHeading}</h2>
-            <section className={styles.videos}>
-            {videos.map((video) => <Card id={video.id} key={video.id} />)}
-            </section>
-        </>
-    );
+  return (
+    <>
+      <h2>{heading}</h2>
+      <section className={styles.videos}>
+        {videos.map((video) => (
+          <Card id={video.id} key={video.id} />
+        ))}
+      </section>
+    </>
+  );
 }
 
 export default VideoList;
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,18 +1,20 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import styles from "./Search.module.css";
-import Container from "../../components/Container"
+import Container from "../../components/Container";
+import VideoList from "../../components/VideoList";
+import videos from "../../json/videos.json"
 
 function Search() {
   return (
     <>
       <Header />
-        <Container>
+      <Container>
         <section className={styles.search}>
-        <h2>Pesquisar</h2>
-        <input type="search" />
-      </section>
-        </Container>
+          <h2>Pesquisar</h2>
+          <VideoList videos={videos} />
+        </section>
+      </Container>
       <Footer />
     </>
   );

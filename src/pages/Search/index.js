92d8057f1e2c2181409337827ffaa699b8b69fec
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-08 23:43:40 -0300

    Modulo search criado e rota também

---
+++ src/pages/Search/index.js
@@ -0,0 +1,11 @@
+import styles from "./Search.module.css"
+
+function Search(){
+    return (
+        <section className={styles.search}>
+            <h2>Pesquisar</h2>
+        </section>
+    );
+}
+
+export default Search;

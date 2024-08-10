f5d30d3138833cf0a937d9a77d4caeea4880f9fb
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-10 00:01:29 -0300

    Implementado forma de exibir a cor do icone para identificar se é ou não é favorito

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
 import iconFavorite from "./favorite.png"
 import iconUnFavorite from "./unfavorite.png"
+import { useFavoritesContext } from "../../contexts/Favorites";
 
 
 function Card({ id }) {
+
+  const { favorite, addFavorite } = useFavoritesContext()
+  const isFavorite = favorite.some((fav) => fav.id === id)
+  const icone = isFavorite ? iconUnFavorite : iconFavorite
   return (
     <section className={styles.card}>
       <Link to={`/watch/${id}`}>
@@ -13,7 +18,11 @@ function Card({ id }) {
           alt="Capa" className={styles.capa} />
       </Link>
       <figure className={styles.icon}>
-        <img src={iconFavorite} alt="Ícone" />
+        <img
+         src={icone}
+         alt="Ícone" 
+         onClick={() => addFavorite({id})}
+         />
       </figure>
     </section>
   );

e3569b8f86cb082c4ae9708aedd0fef140cae4d5
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-09 23:07:51 -0300

    Criação de novo componente/modulo de favoritos

---
+++ src/pages/Favorites/index.js
@@ -0,0 +1,16 @@
+import styles from "./Favorites.module.css";
+import Header from "../../components/Header";
+import Header from "../../components/Container";
+import Header from "../../components/Footer";
+
+function Favorites() {
+  return (
+    <>
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  );
+}
+
+export default Favorites;

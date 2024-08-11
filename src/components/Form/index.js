492b706639099154a8939b2286fb10f09bf1d0f0
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-10 22:43:39 -0300

    Método categories.map refatorado

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,30 +1,35 @@
 import styles from "./Form.module.css";
+import { categories } from "../Category";
 
-function Form(){
-    return (
-        <section className={styles.container}>
-            <h2>Cadastro de vídeos</h2>
-            <form>
-                <div>
-                    <label>URL do vídeo</label>
-                    <input  
-                            type="text"
-                            placeholder="Digite a URL do vídeo"
-                            required="required"
-                            />
-                </div>
-                <div>
-                    <label>Categoria</label>
-                    <select>
-                        <option value="-">Selecione uma Categoria</option>
-                    </select>
-                </div>
-                <div>
-                    <button>Cadastrar</button>
-                </div>
-            </form>
-        </section>
-    );
+function Form() {
+  return (
+    <section className={styles.container}>
+      <h2>Cadastro de vídeos</h2>
+      <form>
+        <div>
+          <label>URL do vídeo</label>
+          <input
+            type="text"
+            placeholder="Digite a URL do vídeo"
+            required="required"
+          />
+        </div>
+        <div>
+          <label>Categoria</label>
+
+          <select>
+            <option value="-">Selecione uma Categoria</option>
+            {categories.map((item) => (
+              <option value={item}>{item}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <button>Cadastrar</button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default Form;

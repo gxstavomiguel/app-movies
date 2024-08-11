958216c2c795217a4a438ccc93625733156b2f9b
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-11 20:48:55 -0300

    Código refatorado para o uso do loader

--- src/components/SearchVideoList/index.js
+++ src/components/SearchVideoList/index.js
@@ -1,39 +1,43 @@
 import { useEffect, useState } from "react";
-import VideoList from "../../components/VideoList"
-import styles from "./SearchVideoList.module.css"
+import VideoList from "../../components/VideoList";
+import styles from "./SearchVideoList.module.css";
 
 //Filtra por categoria/titulo, implementar meio de converter as palvras recebidas no input tudo para minusculas
 
-function filterVideo(videos, searchText){
-    return videos.filter((video) => video.category.includes(searchText) || video.title.includes(searchText))
+function filterVideo(videos, searchText) {
+  return videos.filter(
+    (video) =>
+      video.category.includes(searchText) || video.title.includes(searchText)
+  );
 }
 
+function SearchVideoList({ videos }) {
+  const [searchText, setSearchText] = useState("");
+  const foundVideos = filterVideo(videos, searchText);
 
-function SearchVideoList( {videos }){
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 500);
+  }, []);
 
-    const [ searchText, setSearchText ] = useState("");
-    const foundVideos = filterVideo(videos, searchText)
-
-    const [ loading, setLoading ] = useState(true)
-    useEffect(() => {
-        setTimeout(() => setLoading(false), 500)
-    }, [])
-
-
-    return(
-        <section className={styles.container}>
-            <input
-                type="search"
-                placeholder="Pesquisar..."
-                value={searchText}
-                onChange={e => setSearchText(e.target.value)} 
-            />
-            <VideoList 
-                    videos={foundVideos}
-                    emptyHeading={`Sem vídeos sobre "${searchText}"`}
-                />
-        </section>
-    );
+  return (
+    <section className={styles.container}>
+      <input
+        type="search"
+        placeholder="Pesquisar..."
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      {loading ? (
+        <Loader />
+      ) : (
+        <VideoList
+          videos={foundVideos}
+          emptyHeading={`Sem vídeos sobre "${searchText}"`}
+        />
+      )}
+    </section>
+  );
 }
 
 export default SearchVideoList;

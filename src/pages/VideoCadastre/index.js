16b3d9117dcc764e7785b8b4384945542132bbd9
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-10 22:13:24 -0300

    Modulo VideoCadastre criado

---
+++ src/pages/VideoCadastre/index.js
@@ -0,0 +1,19 @@
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Header from "../../components/Form";
+
+
+function VideoCadastre() {
+  return (
+    <>
+      <Header />
+      <Container>
+        <Form />
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+export default VideoCadastre;

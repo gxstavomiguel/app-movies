d7a0835211870653db63a3280e6ccf950c687ad3
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-10 22:59:53 -0300

    Componente form refatorado

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,22 +2,37 @@ import styles from "./Form.module.css";
 import { categories } from "../Category";
 
 function Form() {
+  const [url, setUrl] = useState("");
+  const [category, setCategory] = useState("");
+
+function onSave(e){
+    //Impede a pagina de recarregar e limpar os campos
+    e.preventDefault()
+    console.log(url, category)
+}
+
   return (
     <section className={styles.container}>
       <h2>Cadastro de vídeos</h2>
-      <form>
+      <form onSubmit={onSave}>
         <div>
           <label>URL do vídeo</label>
           <input
             type="text"
             placeholder="Digite a URL do vídeo"
             required="required"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
           />
         </div>
         <div>
           <label>Categoria</label>
 
-          <select>
+          <select
+            required="required"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value="-">Selecione uma Categoria</option>
             {categories.map((item) => (
               <option value={item}>{item}</option>
